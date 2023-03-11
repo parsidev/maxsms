ae7657b1a8a19d248a04d5a78b9817c672f225cb
Mohammad Reza <programmer.net2009@live.com>
2023-03-11 20:12:55 +0330

    Implement Other Methods

--- src/Parsidev/MaxSms/MaxSms.php
+++ src/Parsidev/MaxSms/MaxSms.php
@@ -29,7 +29,70 @@ class MaxSms
         ];
     }
 
-    public function JobList()
+    public function GetCredit()
+    {
+        return $this->callApi([
+            "uname" => $this->config['username'],
+            "pass" => $this->config['password'],
+            "op" => "credit"
+        ]);
+    }
+
+    public function GetCreditPanel()
+    {
+        return $this->callApi([
+            "uname" => $this->config['username'],
+            "pass" => $this->config['password'],
+            "op" => "usertime"
+        ]);
+    }
+
+    public function GetAccessList()
+    {
+        return $this->callApi([
+            "uname" => $this->config['username'],
+            "pass" => $this->config['password'],
+            "op" => "accesslist"
+        ]);
+    }
+
+    public function GetBookList()
+    {
+        return $this->callApi([
+            "uname" => $this->config['username'],
+            "pass" => $this->config['password'],
+            "op" => "booklist"
+        ]);
+    }
+
+    public function GetLines()
+    {
+        return $this->callApi([
+            "uname" => $this->config['username'],
+            "pass" => $this->config['password'],
+            "op" => "lines"
+        ]);
+    }
+
+    public function GetNews()
+    {
+        return $this->callApi([
+            "uname" => $this->config['username'],
+            "pass" => $this->config['password'],
+            "op" => "news"
+        ]);
+    }
+
+    public function GetInboxList()
+    {
+        return $this->callApi([
+            "uname" => $this->config['username'],
+            "pass" => $this->config['password'],
+            "op" => "inboxlist"
+        ]);
+    }
+
+    public function GetJobList()
     {
         return $this->callApi([
             "uname" => $this->config['username'],
@@ -38,12 +101,49 @@ class MaxSms
         ]);
     }
 
-    public function Credit()
+    public function CountryCity($stateId)
     {
         return $this->callApi([
             "uname" => $this->config['username'],
             "pass" => $this->config['password'],
-            "op" => "credit"
+            "op" => "countrycity",
+            "state_id" => $stateId
+        ]);
+    }
+
+    public function GetDelivery($uniqueId)
+    {
+        return $this->callApi([
+            "uname" => $this->config['username'],
+            "pass" => $this->config['password'],
+            "op" => "delivery",
+            "uinqid" => $uniqueId
+        ]);
+    }
+
+    public function GetMessageStatus($messageId)
+    {
+        return $this->callApi([
+            "uname" => $this->config['username'],
+            "pass" => $this->config['password'],
+            "op" => "checkmessage",
+            "messageid" => $messageId
+        ]);
+    }
+
+    public function BookSend($bookId, $message)
+    {
+        if (!is_array($bookId)) {
+            $bookId = [$bookId];
+        }
+
+        return $this->callApi([
+            "uname" => $this->config['username'],
+            "pass" => $this->config['password'],
+            "bookid" => json_encode($bookId),
+            "message" => $message,
+            "from" => $this->config['from'],
+            "op" => "booksend"
         ]);
     }
 
